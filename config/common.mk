FlatEric0074/vendor_slim

add EsFileExporer free edition
Signed-off-by: martinusbe <martinusbe@gmail.com>

Conflicts:
	config/common.mk

Change-Id: I02f5c9a18e4069be05ec3b4ac91609c87df1037f

config/common.mk
@@ -155,6 +155,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 vendor/tipsy/prebuilt/common/app/Nova33.apk:system/app/Nova33.apk
 
+#Es File Explorer
+PRODUCT_COPY_FILES += \
+    vendor/tipsy/prebuilt/common/app/ESFE.apk:system/app/ESFE.apk
+
 # Adaway
 PRODUCT_COPY_FILES += \
 vendor/tipsy/prebuilt/common/app/adaway.apk:system/app/adaway.apk

r0i/vendor_next_antiguo

config/common.mk
@@ -121,6 +121,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/next/overlay/common
 PRODUCT_COPY_FILES += \
     vendor/next/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
+#Es File Explorer
+PRODUCT_COPY_FILES += \
+    vendor/next/prebuilt/common/app/ESFE.apk:system/app/ESFE.apk
+
 # Layers Manager
 PRODUCT_COPY_FILES += \
 	vendor/next/prebuilt/common/apk/LayersManager.apk:system/priv-app/LayersManager/LayersManager.apk